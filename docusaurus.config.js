fe4f9d73e184acbcab033f5ff41373e246be7d21
Jacob Diaz <hello@jacobdiaz.dev>
2021-09-03 11:25:13 -0500

    Fixed editURL for docs

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -23,7 +23,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
           docs: {
             sidebarPath: require.resolve('./sidebars.js'),
             // Please change this to your repo.
-            editUrl: 'https://https://github.com/jacobdiaz/428landing/',
+            editUrl: 'https://https://github.com/jacobdiaz/428landing/tree/main/',
           },
           theme: {
             customCss: require.resolve('./src/css/custom.css'),
